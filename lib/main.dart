f32b87930a6d8cc41e07ed24ff216e3578851d9e
Praise <praisesamasunga04@gmail.com>
2025-05-18 15:45:41 +0200

    Initialize Firebase in main function and check current user status on app startup.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:zim_shop/providers/app_state.dart';
 import 'package:zim_shop/providers/cart_provider.dart';
 import 'package:zim_shop/providers/theme_provider.dart';
 import 'package:zim_shop/screen/auth/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     MultiProvider(
       providers: [
@@ -24,6 +27,10 @@ class ZimMarketApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
+    
+    // Check if user is already logged in
+    final appState = Provider.of<AppState>(context, listen: false);
+    appState.checkCurrentUser();
     
     return MaterialApp(
       title: 'ZimMarket',
